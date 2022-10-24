e6dd3170f2756d8979270107dbb57bb2e8e69fa0
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-24 17:40:19 -0500

    Created the basemodel class and tests

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.update_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """This method return a string class"""
@@ -20,7 +20,7 @@ class BaseModel:
 
     def save(self):
         """This method update of the instance"""
-        self.update_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """
@@ -29,6 +29,6 @@ class BaseModel:
         """
         dic = self.__dict__
         dic["__class__"] = __class__.__name__
-        dic["update_at"] = datetime.datetime.isoformat(self.update_at)
+        dic["updated_at"] = datetime.datetime.isoformat(self.updated_at)
         dic["created_at"] = datetime.datetime.isoformat(self.created_at)
         return dic
