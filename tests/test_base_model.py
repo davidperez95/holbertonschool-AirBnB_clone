c6079ff99197ef9c5f316e97433dfb6eaa2d8d28
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-10-24 17:46:33 -0500

    test

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -6,11 +6,7 @@ BaseModel = base_model.BaseModel
 
 class TestBaseModel(unittest.TestsCase):
     """Tests for the base class"""
-    
+
     def TestSave(self):
         """Test of the save method"""
-        my_BaseModel = BaseModel()
-        my_1BaseModel = my_BaseModel.updated_at
-        my_BaseModel.save()
-        my_2BaseModel = my_BaseModel.updated_at
-        self.assertNotEqual(my_1BaseModel, my_2BaseModel)
+
