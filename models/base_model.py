e1024d75d5350608045d79b18396f3a8aa71bddc
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-10-26 18:03:24 -0500

    remplace and add new code

--- models/base_model.py
+++ models/base_model.py
@@ -12,17 +12,18 @@ class BaseModel:
     """
     def __init__(self, *args, **kwargs):
         """Instance constructor for the Base Model class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
         if kwargs:
-            if "id" in kwargs:
-                self.id = kwargs["id"]
-            if "created_at" in kwargs:
-                self.created_at = datetime.datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            if "udpated_at" in kwargs:
-                self.updated_at = datetime.datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-        models.storage.new(self)
+            for key, value in kwargs.items():
+                if key == "updated_at" or key == "created_at":
+                    dt_obj = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, dt_obj)
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """This method return a string class"""
