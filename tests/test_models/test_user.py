20cef8170630efb415d7b51b86b04097de7eb51d
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-10-27 11:40:13 -0500

    all test user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 from models.user import User
-from models.base_model import BaseModel
 import unittest
 
 class TestUserClass(unittest.TestCase):
@@ -11,3 +10,21 @@ class TestUserClass(unittest.TestCase):
         u1 = User()
         self.assertEqual(type(u1.email), str)
         self.assertTrue(hasattr(u1, "email"))
+
+    def test_user_password(self):
+        """test to check user password"""
+        User_1 = User()
+        self.assertEqual(type(User_1.password), str)
+        self.assertTrue(hasattr(User_1, "password"))
+        
+    def test_user_first_name(self):
+        """test to check user first name"""
+        User_1 = User()
+        self.assertEqual(type(User_1.first_name), str)
+        self.assertTrue(hasattr(User_1, "first_name"))
+
+    def test_user_last_name(self):
+        """test to check user last_name"""
+        User_1 = User()
+        self.assertEqual(type(User_1.last_name), str)
+        self.assertTrue(hasattr(User_1, "last_name"))
