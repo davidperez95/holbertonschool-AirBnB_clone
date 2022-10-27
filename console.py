59b60bb32d6954e0e6e04ec1e7ddd38f3c39aed8
ManuRodriguezC <estebanclimb@gmail.com>
2022-10-27 16:56:50 -0500

    sometasks avanced

--- console.py
+++ console.py
@@ -9,14 +9,18 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from models import storage
-list_class = {"BaseModel": BaseModel, "User": User, "State": State,
-                "City": City, "Amenity": Amenity, "Place": Place, "Review": Review}
+list_class = {"BaseModel": BaseModel,
+              "User": User, "State": State,
+              "City": City,
+              "Amenity": Amenity,
+              "Place": Place,
+              "Review": Review}
 
 
 class HBNBCommand(cmd.Cmd):
     """Entry point for the console"""
-
     prompt = "(hbnb) "
+
     def do_EOF(self, line):
         """Implement the EOF"""
         return True
@@ -35,7 +39,7 @@ class HBNBCommand(cmd.Cmd):
         if line == "":
             print("** class name missing **")
             return False
-        if not line in list_class:
+        if line not in list_class:
             print("** class doesn't exist **")
             return False
         for key, value in list_class.items():
@@ -146,6 +150,76 @@ class HBNBCommand(cmd.Cmd):
                 setattr(value, args[2], args[3].strip('"'))
                 storage.save()
                 return False
+
+    def default(self, line):
+        """Retrieve all instances of a class by using: <class.name>"""
+        if line == "":
+            print("** command name missing **")
+            return False
+
+        all_dict = []
+        print_list = []
+        all_dict = storage.all()
+        count = 0
+        name_class = ""
+        if line.startswith("BaseModel"):
+            name_class = "BaseModel"
+        elif line.startswith("User"):
+            name_class = "User"
+        elif line.startswith("State"):
+            name_class = "State"
+        elif line.startswith("City"):
+            name_class = "City"
+        elif line.startswith("Amenity"):
+            name_class = "Amenity"
+        elif line.startswith("Place"):
+            name_class = "Place"
+        elif line.startswith("Review"):
+            name_class = "Review"
+
+        if line == name_class:
+            print("** command name missing **")
+            print(all_dict)
+            return False
+
+        for key, value in all_dict.items():
+            if name_class in key:
+                print_list.append(str(value))
+                count += 1
+
+        command = line.strip(name_class)
+        if command == ".all()":
+            print(print_list)
+            return False
+        if command == ".count":
+            print(count)
+            return False
+        if command.startswith(".show"):
+            id_model = command.strip('.show("")')
+            if id_model == "":
+                print("** instance id missing **")
+                return False
+            for key in all_dict.keys():
+                if f"{name_class}.{id_model}" == key:
+                    print(all_dict[key])
+                    return False
+            print("** no instance found **")
+            return False
+        if command.startswith(".destroy"):
+            id_model = command.strip('.destroy("")')
+            if id_model == "":
+                print("** instance id missing **")
+                return False
+            for key in all_dict.keys():
+                if f"{name_class}.{id_model}" == key:
+                    del all_dict[key]
+                    storage.save()
+                    return False
+            print("** no instance found **")
+            return False
+        print(f"*** Unknown syntax: {line}")
+        return False
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
