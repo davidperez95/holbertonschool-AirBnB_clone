deb539eb02649c575434a7876825b2cddf9460b3
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-10-27 13:24:35 -0500

    a little error in amenity test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -7,7 +7,7 @@ class TestAmenityClass(unittest.TestCase):
     def test_amenity_name(self):
         """test to check user name"""
         User_1 = Amenity()
-        self.assertEqual(type(User_1.id), str)
+        self.assertEqual(type(User_1.name), str)
         self.assertTrue(hasattr(User_1, "name"))
 
 if __name__ == "__main__":
